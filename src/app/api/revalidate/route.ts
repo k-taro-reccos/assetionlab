061d59c71d139ee732cc5d7792552fee206c2843
k-taro-reccos <tarou3806@gmail.com>
2023-05-30 20:14:53 +0900

    🐛

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,26 +1,26 @@
 import { NextResponse } from "next/server"
-import {  revalidateTag } from "next/cache"
-// import { Post } from "types"
+import { revalidateTag } from "next/cache"
+import { Post } from "types"
 
 export const runtime = "edge"
 
-// type Data = {
-//   contents: Post[]
-// }
+type Data = {
+  contents: Post[]
+}
 
 export async function POST() {
-  // const res = await fetch(
-  //   "https://finance-blog.microcms.io/api/v1/blogs?limit=999",
-  //   {
-  //     headers: {
-  //       "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY as string,
-  //     },
-  //   }
-  // )
-  // if (!res.ok) {
-  //   throw new Error("Failed to fetch data")
-  // }
-  // const data: Data = await res.json()
+  const res = await fetch(
+    "https://finance-blog.microcms.io/api/v1/blogs?limit=999",
+    {
+      headers: {
+        "X-MICROCMS-API-KEY": process.env.MICROCMS_API_KEY as string,
+      },
+    }
+  )
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  const data: Data = await res.json()
   revalidateTag("posts")
-  return NextResponse.json({revalidated: true})
+  return NextResponse.json(data.contents)
 }
