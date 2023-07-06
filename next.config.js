c1b48b9a7431663a69eb2c9cf32b0fa27b113c5f
k-taro-reccos <tarou3806@gmail.com>
2023-07-06 19:32:04 +0900

    ✨ プレビュー環境の実装

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ["source.unsplash.com", "images.microcms-assets.io"],
   },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 }
 
 module.exports = nextConfig
