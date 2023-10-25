092ff6a0feac98e426133d081744cd005cd8f8ea
k-taro-reccos <tarou3806@gmail.com>
2023-10-25 09:13:16 +0900

    🎨 再検証のコード修正

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,14 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { revalidateTag } from "next/cache"
 
 export const runtime = "edge"
 
-export async function POST() {
-  revalidateTag("post")
+export async function POST(request: NextRequest) {
+  const tag = request.nextUrl.searchParams.get('tag')
+  
+  if (!tag)
+    return NextResponse.json({ message: "No tag provided" }, { status: 400 })
+
+  revalidateTag(tag)
   return NextResponse.json({ revalidated: true })
 }
